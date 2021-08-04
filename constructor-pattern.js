7c00abd98a30332ef5b60f2c941a0e32b8cd82d7
jasmine <jasminetripti5@gmail.com>
2021-08-04 16:07:34 +0600

    constructor-example-1

--- constructor-pattern.js
+++ constructor-pattern.js
@@ -1,4 +1,6 @@
-function person(){
-    console.log("i hv construted")
+function person(name,age){
+    this.name = name;
+    this.age = age;
 }
-var p1 = new person()
+var p1 = new person('jasmine', 22)
+console.log(p1.name +' age:' +p1.age)

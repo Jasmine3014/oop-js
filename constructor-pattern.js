f346f1e58a0b6808d344694fea8d0156a73a6311
jasmine <jasminetripti5@gmail.com>
2021-08-08 16:36:04 +0600

    constructor

--- constructor-pattern.js
+++ constructor-pattern.js
@@ -1,11 +1,11 @@
-// function person(name,age){
-//     this.name = name;
-//     this.age = age;
-// }
-// var p1 = new person('jasmine', 22)
-// console.log(p1.name +' age:' +p1.age)
+function person(name,age){
+    this.name = name;
+    this.age = age;
+}
+var p1 = new person('jasmine', 22)
+console.log(p1.name +' age:' +p1.age)
 
-//stored im array
+//stored in array
 function person(name,age){
     this.name = name;
     this.age = age;
@@ -14,4 +14,17 @@ var p1 = new person('jasmine', 22)
 var p2 = new person('akik', 19)
 var p3 = new person('jasmine', 22)
 var people = [p1,p2,p3]
-console.log(people)
+// console.log(people)
+people.forEach (function (person ){
+    console.log('age :' +person.age)
+})
+
+//another example
+function Book(name,author,published){
+    this.name =name;
+    this.author = author ;
+    this.published = published;
+
+}
+var book = new Book('zoology','jasmine',2014)
+console.log(constructor.book)

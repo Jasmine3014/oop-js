412837206fa28e30e0371b8b1241881ee9c9610a
jasmine <jasminetripti5@gmail.com>
2021-08-08 14:14:13 +0600

    constructor-example-array

--- constructor-pattern.js
+++ constructor-pattern.js
@@ -1,6 +1,17 @@
+// function person(name,age){
+//     this.name = name;
+//     this.age = age;
+// }
+// var p1 = new person('jasmine', 22)
+// console.log(p1.name +' age:' +p1.age)
+
+//stored im array
 function person(name,age){
     this.name = name;
     this.age = age;
 }
 var p1 = new person('jasmine', 22)
-console.log(p1.name +' age:' +p1.age)
+var p2 = new person('akik', 19)
+var p3 = new person('jasmine', 22)
+var people = [p1,p2,p3]
+console.log(people)

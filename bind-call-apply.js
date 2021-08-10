5a99fee88bcd69755ec4f830a5da6530b5251425
jasmine <jasminetripti5@gmail.com>
2021-08-10 17:40:26 +0600

    call-method

--- bind-call-apply.js
+++ bind-call-apply.js
@@ -1,11 +1,13 @@
 function printMe(){
-    console.log('hello' + this.name)
+    console.log('hello ,' + this.name)
 }
 var obj ={
     name:'jas ',age:20
 }
 var obj2 ={
-    name:'jas ',age:20
+    name:'jasmine ',age:20
 }
-var binded = printMe.bind(obj);
-binded()
+// var binded = printMe.bind(obj);
+// binded()
+//call method
+printMe.call(obj2)

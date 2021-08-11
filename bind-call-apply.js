4c576de36543fbb3896fc41b3c15465ed599d086
jasmine <jasminetripti5@gmail.com>
2021-08-11 17:51:37 +0600

    two-argu call-method

--- bind-call-apply.js
+++ bind-call-apply.js
@@ -9,5 +9,21 @@ var obj2 ={
 }
 // var binded = printMe.bind(obj);
 // binded()
+
 //call method
 printMe.call(obj2)
+
+//call method with two argument
+function add(a,b){
+    return (a+b)*this.c;
+}
+
+var obj3={
+    c:2
+}
+var obj3={
+    c:3
+}
+var res = add.call(obj3,10,3)
+console.log (res);
+
